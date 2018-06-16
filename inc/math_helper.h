cceca543e05457d7204c8f1595afe95e2254e521
James Jackson <superjax08@gmail.com>
2018-06-16 16:25:27 -0600

    working sampler code, patch extraction projection and error jacobian

---
+++ inc/math_helper.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "math.h"
+#include "quat.h"
+
+#include <Eigen/Core>
+
+static const Eigen::Matrix<double, 2, 3> I_2x3 = [] {
+  Eigen::Matrix<double, 2, 3> tmp;
+  tmp << 1.0, 0, 0,
+         0, 1.0, 0;
+  return tmp;
+}();
+
+static const Eigen::Matrix3d I_3x3 = [] {
+  Eigen::Matrix3d tmp = Eigen::Matrix3d::Identity();
+  return tmp;
+}();
+
+static const Eigen::Matrix2d I_2x2 = [] {
+  Eigen::Matrix2d tmp = Eigen::Matrix2d::Identity();
+  return tmp;
+}();
+
+
+static const Eigen::Vector3d e_x = [] {
+  Eigen::Vector3d tmp;
+  tmp << 1.0, 0, 0;
+  return tmp;
+}();
+
+static const Eigen::Vector3d e_y = [] {
+  Eigen::Vector3d tmp;
+  tmp << 0, 1.0, 0;
+  return tmp;
+}();
+
+static const Eigen::Vector3d e_z = [] {
+  Eigen::Vector3d tmp;
+  tmp << 0, 0, 1.0;
+  return tmp;
+}();
+
+void removeRow(Eigen::MatrixXd& matrix, unsigned int rowToRemove);
+void removeColumn(Eigen::MatrixXd& matrix, unsigned int colToRemove);
+
+inline Eigen::Matrix3d skew(const Eigen::Vector3d v)
+{
+  Eigen::Matrix3d mat;
+  mat << 0.0, -v(2), v(1),
+         v(2), 0.0, -v(0),
+         -v(1), v(0), 0.0;
+  return mat;
+}
+
+inline Eigen::Matrix<double, 3, 2> T_zeta(quat::Quat q)
+{
+  return q.doublerot(I_2x3.transpose());
+}
+
+inline Eigen::Vector2d q_feat_boxminus(quat::Quat q0, quat::Quat q1)
+{
+  Eigen::Vector3d zeta0 = q0.rota(e_z);
+  Eigen::Vector3d zeta1 = q1.rota(e_z);
+
+  Eigen::Vector2d dq;
+  if ((zeta0 - zeta1).norm() > 1e-16)
+  {
+    Eigen::Vector3d v = zeta1.cross(zeta0);
+    v /= v.norm();
+    double theta = std::acos(zeta1.dot(zeta0));
+    dq = theta * T_zeta(q1).transpose() * v;
+  }
+  else
+  {
+    dq.setZero();
+  }
+  return dq;
+}
+
+inline quat::Quat q_feat_boxplus(quat::Quat q, Eigen::Vector2d dq)
+{
+  return quat::Quat::exp(T_zeta(q) * dq) * q;
+}
+
+
+void concatenate_SE2(Eigen::Vector3d& T1, Eigen::Vector3d& T2, Eigen::Vector3d& Tout);
+void concatenate_edges(const Eigen::Matrix<double,7,1>& T1, const Eigen::Matrix<double,7,1>& T2, Eigen::Matrix<double,7,1>& Tout);
+const Eigen::Matrix<double,7,1> invert_edge(const Eigen::Matrix<double,7,1>& T1);
+void invert_SE2(Eigen::Vector3d& T, Eigen::Vector3d& Tout);
+
+template <typename T>
+int sign(T in)
+{
+  return (in >= 0) - (in < 0);
+}
+
---
+++ src/cvtest.cpp
@@ -0,0 +1,264 @@
+﻿#include "Eigen/Core"
+#include "Eigen/Geometry"
+#include "Eigen/Dense"
+
+#include "quat.h"
+#include "math_helper.h"
+
+#include <deque>
+#include <set>
+#include <unordered_map>
+#include <functional>
+#include <fstream>
+#include <chrono>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace Eigen;
+using namespace cv;
+using namespace quat;
+
+#define PATCH_SIZE 25
+#define PYRAMID_LEVELS 1
+
+typedef Matrix<double, 6, 1> uVector;
+typedef Matrix<double, 7, 1> eVector;
+typedef Matrix<double, 4, 1> zVector;
+typedef Matrix<float, 2, 1> pixVector;
+typedef Matrix<uint8_t, PATCH_SIZE,PATCH_SIZE> patchMat;
+typedef Matrix<uint8_t, PATCH_SIZE*PATCH_SIZE*PYRAMID_LEVELS, 1> multiPatchVectorI;
+typedef Matrix<float, PATCH_SIZE, PATCH_SIZE*PYRAMID_LEVELS> multiPatchMatrixf;
+typedef Matrix<float, PATCH_SIZE*PATCH_SIZE*PYRAMID_LEVELS, 1> multiPatchVectorf;
+typedef Matrix<float, PATCH_SIZE*PATCH_SIZE*PYRAMID_LEVELS, 2> multiPatchJacMatrix;
+
+Mat img_[PYRAMID_LEVELS];
+
+Vector2d cam_center_;
+Matrix<double, 2, 3> cam_F_;
+
+
+void proj(Quat& qz, Vector2d& eta, Matrix2d& jac)
+{
+  Vector3d zeta = qz.rota(e_z);
+  Matrix3d sk_zeta = skew(zeta);
+  double ezT_zeta = e_z.transpose() * zeta;
+  MatrixXd T_z = T_zeta(qz);
+  
+  eta = cam_F_ * zeta / ezT_zeta + cam_center_;
+  jac = -cam_F_ * ((sk_zeta * T_z)/ezT_zeta - (zeta * e_z.transpose() * sk_zeta * T_z)/(ezT_zeta*ezT_zeta));
+}
+
+void multiLvlPatch(const pixVector &eta, multiPatchVectorf& dst)
+{
+  float x = eta(0,0);
+  float y = eta(1,0);
+  Size sz(PATCH_SIZE, PATCH_SIZE);
+  for (int i = 0; i < PYRAMID_LEVELS; i++)
+  {
+    Mat ROI;
+  getRectSubPix(img_[i], sz, Point2f(x,y), ROI, CV_32FC1);
+  x /= 2.0;
+    y /= 2.0;
+    
+    // Convert to Eigen, but just the part that we want to update
+    const Mat _dst(PATCH_SIZE, PATCH_SIZE, CV_32FC1, dst.data() + i * PATCH_SIZE*PATCH_SIZE, PATCH_SIZE * sizeof(float));
+    if( ROI.type() == _dst.type() )
+        transpose(ROI, _dst);
+    else
+    {
+        ROI.convertTo(_dst, _dst.type());
+        transpose(_dst, _dst);
+    }
+  }
+}
+
+
+void sample_pixels(Quat& qz, const Matrix2d& cov, std::vector<pixVector>& eta)
+{
+  (void)qz;
+  (void)cov;
+  (void)eta;
+  
+  Vector2d eta0;
+  Matrix2d eta_jac;
+  proj(qz, eta0, eta_jac);
+  
+  Eigen::SelfAdjointEigenSolver<Matrix2d> eigensolver((eta_jac.transpose() * cov * eta_jac));
+  Vector2d e = eigensolver.eigenvalues();
+  Matrix2d v = eigensolver.eigenvectors();
+  Vector2d::Index max_id, dummy;
+  double max_e = e.maxCoeff(&max_id, &dummy);
+  double min_e = e(1 - max_id, 0);
+  
+  if (max_id == 1)
+    v.transposeInPlace();
+  
+  double a = 2.0*std::sqrt(std::abs(max_e));
+  double b = 2.0*std::sqrt(std::abs(min_e));
+  
+  for (float y = 0; y < std::round(b); y += PATCH_SIZE * 0.9)
+  {
+    float xmax = (a/b) * std::sqrt(b*b - y*y);
+    for (float x = 0; x < xmax; x += PATCH_SIZE * 0.9)
+    {
+      float t = std::atan2(y, x);
+      float ct = std::cos(t);
+      float st = std::sin(t);
+      if ((y*y + x*x) < (a*a*ct*ct + b*b*st*st))
+      {
+        Vector2d p;
+        p << x, y;
+        Vector2f pt = (v * p + eta0).cast<float>();
+        if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
+          eta.push_back(pt);
+        if (x != 0)
+        {
+          p(0, 0) *= -1;
+          pt = (v * p + eta0).cast<float>();
+          if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
+            eta.push_back(pt);
+        }
+        if (y != 0)
+        {
+          p(1, 0) *= -1;
+          pt = (v * p + eta0).cast<float>();
+          if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
+            eta.push_back(pt);
+          if (x != 0)
+          {
+            p(0, 0) *= -1;
+            pt = (v * p + eta0).cast<float>();
+            if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
+              eta.push_back(pt);
+          }
+        } 
+      }
+    }
+  }
+    
+}
+
+
+void patch_error(const pixVector &etahat, const multiPatchVectorf &I0, multiPatchVectorf &e, multiPatchJacMatrix &J)
+{
+  multiPatchVectorf Ip, Im;
+  multiLvlPatch(etahat, Ip);
+  
+  e = Ip - I0;
+  Matrix2f eye2 = Matrix2f::Identity();
+  
+  // Perform central differencing
+  for (int i = 0; i < 2; i++)
+  {
+    multiLvlPatch(etahat + eye2.col(i), Ip);
+    multiLvlPatch(etahat - eye2.col(i), Im);
+    J.col(i) = ((Ip - I0) - (Im - I0))/2.0;
+  }
+}
+
+void multiLvlPatchSideBySide(multiPatchVectorf& src, multiPatchMatrixf& dst)
+{
+   dst = Map<multiPatchMatrixf>(src.data());
+}
+
+void multiLvlPatchToCv(multiPatchVectorf& src, Mat& dst)
+{
+  multiPatchMatrixf side_by_side;
+  multiLvlPatchSideBySide(src, side_by_side);
+  eigen2cv(side_by_side, dst);
+}
+
+int main()
+{  
+  
+  Mat rgb = cv::imread("/home/superjax/.ros/cache/RGB00558.bmp");
+  Mat grey;
+  cvtColor(rgb, grey, COLOR_RGB2GRAY);
+  
+  // Build multilevel image
+  img_[0] = grey;
+  for (int i = 1; i < PYRAMID_LEVELS; i++)
+  {
+    pyrDown( img_[i-1], img_[i], Size( img_[i-1].cols/2, img_[i-1].rows/2 ) );
+  }
+  // Convert the image to a float image
+  for (int i = 0; i < PYRAMID_LEVELS; i++)
+  {
+    img_[i].convertTo(img_[i], CV_32FC1);
+  }
+  
+  pixVector eta;
+  eta << 253.3, 420.63827;
+ 
+  multiPatchVectorf patch;
+  
+  // Extract a Multi-Level Patch
+  multiLvlPatch(eta, patch);
+  
+  // Show the levels side by side
+  multiPatchMatrixf side_by_side;
+  multiLvlPatchSideBySide(patch, side_by_side);
+  
+  // Convert Multi level patch to cv Mat and write to file
+  Mat cv_side_by_side;
+  multiLvlPatchToCv(patch, cv_side_by_side);
+  imwrite("/home/superjax/test/side_by_side.png", cv_side_by_side);
+  
+  cam_center_ << 320.0, 240.0;
+  cam_F_ << 250.0, 0.0, 0.0,   0.0, 250.0, 0.0;
+  
+  // TEST patch_error jacobian calculation by performing Gauss-Newton Optimization on
+  // a sample problem
+  multiPatchJacMatrix J;
+  multiPatchVectorf e;
+  pixVector etahat;
+  etahat << 247.0, 412.0;
+  patch_error(etahat, patch, e, J);
+  
+  Quat qz = Quat::Identity();
+  Matrix2d cov;
+  Vector2d dq;
+  dq << -0.1, 0.45;
+  cov << 0.02, 0.03, 0.03, 0.01;
+  qz = q_feat_boxplus(qz, dq);
+  
+  std::vector<pixVector> pix;
+  sample_pixels(qz, cov, pix);
+  
+  Mat sampled;
+  cvtColor(grey, sampled, COLOR_GRAY2BGR);
+  double alpha = 0.3;
+  for (int i = 0; i < pix.size(); i++)
+  {
+    cv::Mat roi = sampled(Rect(Point(pix[i](0,0), pix[i](1,0)), Size(PATCH_SIZE, PATCH_SIZE)));
+    cv::Mat color(roi.size(), CV_8UC3, cv::Scalar(255, 0, 255)); 
+    cv::addWeighted(color, alpha, roi, 1.0 - alpha , 0.0, roi); 
+  }
+  imwrite("/home/superjax/test/sampled.png", sampled);
+ 
+  int iter = 0;
+  double prev_err = INFINITY;
+  double current_err = e.norm();
+  while (current_err > 1e-3 && iter < 100)
+  {
+    cout << "iter: " << iter << " e = " << current_err << " etahat = " << etahat.transpose() << std::endl;
+    etahat = etahat - (J.transpose() * J).inverse() * J.transpose() * e;
+    patch_error(etahat, patch, e, J);
+    iter++;
+    prev_err = current_err;
+    current_err = e.norm();
+  }
+  
+  std::cout << "optimization finished\n";
+  std::cout << "iter: " << iter << " e = " << current_err << " etahat = " << etahat.transpose() << std::endl;
+  
+  
+  
+  
+  waitKey(0);  
+}
+
