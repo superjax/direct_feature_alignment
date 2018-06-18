9d949d11cc9417f09d4bf250297da7b34686ebba
James Jackson <superjax08@gmail.com>
2018-06-18 13:13:46 -0600

    plotting stuff

--- src/cvtest.cpp
+++ src/cvtest.cpp
@@ -15,6 +15,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/core/eigen.hpp>
+#include <opencv2/aruco.hpp>  
 #include <iostream>
 #include <random>
 
@@ -54,49 +55,35 @@ void proj(Quat qz, Vector2f& eta, Matrix2f& jac)
   jac = (-cam_F_ * ((sk_zeta * T_z)/ezT_zeta - (zeta * e_z.transpose() * sk_zeta * T_z)/(ezT_zeta*ezT_zeta))).cast<float>();
 }
 
-Size sz(PATCH_SIZE, PATCH_SIZE);
-Mat patch_ROI;
 void multiLvlPatch(const pixVector &eta, multiPatchVectorf& dst)
 {
+  Size sz(PATCH_SIZE, PATCH_SIZE);
   float x = eta(0,0);
   float y = eta(1,0);
   for (int i = 0; i < PYRAMID_LEVELS; i++)
   {
-    
     const Mat _dst(PATCH_SIZE, PATCH_SIZE, CV_32FC1, dst.data() + i * PATCH_SIZE*PATCH_SIZE, PATCH_SIZE * sizeof(float));
     getRectSubPix(img_[i], sz, Point2f(x,y), _dst);
     x /= 2.0;
     y /= 2.0;
-    
-    // Convert to Eigen, but just the part that we want to update
-//    if( patch_ROI.type() == _dst.type() )
-//      transpose(patch_ROI, _dst);
-//    else
-//    {
-//      patch_ROI.convertTo(_dst, _dst.type());
-//      transpose(_dst, _dst);
-//    }
   }
 }
 
 Eigen::SelfAdjointEigenSolver<Matrix2f> eigensolver;
 void sample_pixels(Quat& qz, const Matrix2f& cov, std::vector<Vector2f>& eta)
-{
-  (void)qz;
-  (void)cov;
-  (void)eta;
-  
+{  
   Vector2f eta0;
   Matrix2f eta_jac;
   proj(qz, eta0, eta_jac);
   
-  eigensolver.computeDirect((eta_jac.transpose() * cov * eta_jac));
+  Matrix2f P = eta_jac * cov * eta_jac.transpose();
+  eigensolver.computeDirect(P);
   Vector2f e = eigensolver.eigenvalues();
   Matrix2f v = eigensolver.eigenvectors();
-  
   double a = 3.0*std::sqrt(std::abs(e(0,0)));
   double b = 3.0*std::sqrt(std::abs(e(1,0)));
   
+  /// TODO: Spiral out from center, instead of along axis to increase search efficiency later
   Vector2f p, pt;
   for (float y = 0; y < b; y += PATCH_SIZE * 1.0)
   {
@@ -104,12 +91,20 @@ void sample_pixels(Quat& qz, const Matrix2f& cov, std::vector<Vector2f>& eta)
     float xmax = (a/b) * std::sqrt(b*b - y*y);
     for (float x = 0; x < xmax; x += PATCH_SIZE * 1.0)
     {
-      float t = std::atan2(y, x);
-      float ct = std::cos(t);
-      float st = std::sin(t);
-      if ((y*y + x*x) < (a*a*ct*ct + b*b*st*st))
+      p(0,0) = x;
+      pt = (v * p + eta0);
+      if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
+        eta.push_back(pt);
+      if (x != 0)
       {
-        p(0,0) = x;
+        p(0, 0) *= -1;
+        pt = (v * p + eta0);
+        if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
+          eta.push_back(pt);
+      }
+      if (y != 0)
+      {
+        p(1, 0) *= -1;
         pt = (v * p + eta0);
         if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
           eta.push_back(pt);
@@ -120,20 +115,6 @@ void sample_pixels(Quat& qz, const Matrix2f& cov, std::vector<Vector2f>& eta)
           if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
             eta.push_back(pt);
         }
-        if (y != 0)
-        {
-          p(1, 0) *= -1;
-          pt = (v * p + eta0);
-          if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
-            eta.push_back(pt);
-          if (x != 0)
-          {
-            p(0, 0) *= -1;
-            pt = (v * p + eta0);
-            if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img_[0].cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img_[0].rows)
-              eta.push_back(pt);
-          }
-        } 
       }
     }
   }
@@ -169,17 +150,14 @@ void multiLvlPatchToCv(multiPatchVectorf& src, Mat& dst)
   eigen2cv(side_by_side, dst);
 }
 
-void drawPatch(int i, Mat& img, const Vector2f& pt, const Scalar& col, double alpha=0.3)
+void drawPatch(Mat& img, const Vector2f& pt, const Scalar& col, double alpha=0.3)
 {
-#if 0
   if (pt(0,0) > PATCH_SIZE && pt(0,0) + PATCH_SIZE < img.cols && pt(1,0) > PATCH_SIZE && pt(1,0) + PATCH_SIZE < img.rows)
   {
     cv::Mat roi = img(Rect(Point(pt(0,0), pt(1,0)), Size(PATCH_SIZE, PATCH_SIZE)));
     cv::Mat color(roi.size(), CV_8UC3, col); 
     cv::addWeighted(color, alpha, roi, 1.0 - alpha , 0.0, roi); 
-    imwrite("/home/superjax/test/sampled" + std::to_string(i) + ".png", img);
   }
-#endif
 }
 
 int main()
@@ -199,7 +177,6 @@ int main()
   {
     img_[i].convertTo(img_[i], CV_32FC1);
   }
-  patch_ROI = Mat(img_[0].rows, img_[0].cols, CV_32FC1);
   
   // Make up an intrinsic matrix
   cam_center_ << 320.0, 240.0;
@@ -210,13 +187,16 @@ int main()
   std::normal_distribution<double> distribution(0, 1.0);
   std::uniform_real_distribution<double> uniform(-1.0, 1.0);
   
+  std::vector<pixVector> pix;
+  std::vector<pixVector> pix_copy;
+  pix.reserve(100);
+  pix_copy.reserve(100);
+  
   int timitsum = 0;
   int timitcount = 0;
   int successcount =0;
   for (int timit = 0; timit < 100; timit++)
   {
-    
-    
     // Plot samples
     Mat sampled;
     cvtColor(grey, sampled, COLOR_GRAY2BGR);
@@ -233,7 +213,7 @@ int main()
     Matrix2f dummy;
     Vector2f eta;
     multiPatchVectorf patch;
-    dq << 0.3*uniform(generator), 0.3*uniform(generator);
+    dq << 0.5*uniform(generator), 0.4*uniform(generator);
     qz = q_feat_boxplus(qz, dq);
     proj(qz, eta, cov);
     
@@ -246,54 +226,40 @@ int main()
     //    imwrite("side-by-side.png", side_by_side);
     
     // Covariance of measurement
-    cov << 0.0005, 0.0001, 0.0001, 0.0005;
+    float dx = (0.002 * uniform(generator)) + 0.0025;
+    float dy = (0.002 * uniform(generator)) + 0.0025;
+    float dxdy = (0.001 * uniform(generator));
+    cov << dx, dxdy, dxdy, dy;
     eigensolver.computeDirect(cov);
     Vector2f l = eigensolver.eigenvalues();
     Matrix2f v = eigensolver.eigenvectors();
     
     // Sample from the cov
     Vector2f sample;
-    sample << std::sqrt(l(0,0)) * distribution(generator),
-              std::sqrt(l(1,0)) * distribution(generator);
+    sample << std::sqrt(std::abs(l(0,0))) * distribution(generator),
+              std::sqrt(std::abs(l(1,0))) * distribution(generator);
     Quat qzhat = q_feat_boxplus(qz, (v * sample).cast<double>());  
     proj(qzhat, etahat, dummy);
     
     if (etahat(0,0) < PATCH_SIZE || etahat(0,0) + PATCH_SIZE > img_[0].cols || etahat(1,0) < PATCH_SIZE || etahat(1,0) + PATCH_SIZE > img_[0].rows)
       continue;
     
-    // To compare, also plot the 2 stdev patches from the qz covariance
-    //  for (int i =0; i < 2; i++)
-    //  {
-    //    Vector2d p1, p2;
-    //    proj(q_feat_boxplus(qzhat, 2.0*std::sqrt(l(i,0)) * v.col(i)), p1, dummy);
-    //    proj(q_feat_boxplus(qzhat, -2.0*std::sqrt(l(i,0)) * v.col(i)), p2, dummy);
-    //    drawPatch(sampled, p1, Scalar(255, 0, 0));
-    //    drawPatch(sampled, p2, Scalar(255, 0, 0));
-    //  } 
-    
-    std::vector<pixVector> pix;
-    std::vector<pixVector> pix_copy;
-    pix.reserve(100);
-    pix_copy.reserve(100);
+
+    pix.clear();
+    pix_copy.clear();
     auto start = std::chrono::high_resolution_clock::now();
     // Sample some pixels
     sample_pixels(qzhat, cov, pix);
-    //  for (int i = 0; i < pix.size(); i++)
-    //  {
-    //    drawPatch(sampled, pix[i], Scalar(255, 0, 255));
-    //  }
     
-    double min_error = INFINITY;
     int min_patch_idx = 0;
     double tol = 1;
     for (int i = 0; i < pix.size(); i++)
     {
       pix_copy.push_back(pix[i]);
-      //    std::cout << " Patch " << i << "  ====================================" << std::endl;
       int iter = 0;
       bool done = false;
       double prev_err = INFINITY;
-      double current_err = e.norm();
+      double current_err = INFINITY;
       do
       {
         patch_error(pix[i], patch, e, J);
@@ -323,29 +289,41 @@ int main()
     
     // Call it a success if we matched to within 1 pixel
     double actual_error = (pix[min_patch_idx] - eta).norm();    
-    std::cout << ((actual_error < 1) ? "success - " : "failed - ") << "patch: " << min_patch_idx << " err: " << actual_error 
-              << ", took " << us << "us" << std::endl;
+    std::cout << timit << ((actual_error < 1) ? " success - " : " failed - ") << "patch: " << min_patch_idx << " err: " << actual_error 
+              << ", took " << us << "us" << " cov = [" << cov(0,0) << ", " << cov(0,1) << "; " << cov(1,0) << ", " << cov(1,1) << "]" << std::endl;
     
     if (actual_error < 1)
       successcount++;
     else
     {
-      for (int i = 0; i < pix.size(); i++)
-      {
-        drawPatch(timit, sampled, pix[i], Scalar(255, 0, 255));
-        drawPatch(timit, sampled, pix_copy[i], Scalar(255, 0, 0));
-      }
-      drawPatch(timit, sampled, pix[min_patch_idx], Scalar(0, 255, 255), 1.0);
-      drawPatch(timit, sampled, eta, Scalar(0, 255, 0), 0.5);
     }
+
+    for (int i = 0; i < pix_copy.size(); i++)
+      drawPatch(sampled, pix_copy[i], Scalar(255, 0, 0)); // original patch locations (blue)
+    for (int i = 0; i < pix.size(); i++)
+      drawPatch(sampled, pix[i], Scalar(255, 0, 255)); // where the patches went (magenta)
+
+    // To compare, also plot the 2 stdev patches from the qz covariance
+    for (int i =0; i < 2; i++)
+    {
+      Vector2f p1, p2;
+      proj(q_feat_boxplus(qzhat, 2.0*std::sqrt(l(i,0)) * v.cast<double>().col(i)), p1, dummy);
+      proj(q_feat_boxplus(qzhat, -2.0*std::sqrt(l(i,0)) * v.cast<double>().col(i)), p2, dummy);
+      drawPatch(sampled, p1, Scalar(0, 0, 255)); // qz distribution (red)
+      drawPatch(sampled, p2, Scalar(0, 0, 255));
+    } 
+
+    drawPatch(sampled, pix[min_patch_idx], Scalar(0, 255, 255), 0.8); // optimized guess (yellow)
+    drawPatch(sampled, eta, Scalar(0, 255, 0), 0.5); // (actual answer (green))
+
+    
+    imwrite("/home/superjax/test/sampled" + std::to_string(timit) + ".png", sampled);
     
     timitcount++;
     timitsum += us;
   }
   std::cout << "average " << ((double) timitsum / (double) timitcount)*1e-6 << " seconds, success = " << (double)successcount / (double)timitcount << std::endl;
-  
-  double alpha = 0.3;
-  
+   
   
   waitKey(0);  
 }
